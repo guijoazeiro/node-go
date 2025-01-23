ab5f65c408e6de4f9992d73086aca6164a9a0d0b
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2025-01-23 14:16:15 -0300

    fix eslint error

--- nodejs-api/src/db/index.ts
+++ nodejs-api/src/db/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Pool, PoolClient, QueryResult, QueryResultRow } from 'pg';
 import { env } from '../config/env';
 import logger from '../config/logger';
